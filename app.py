e68b54a57828d1711b62d808c09f8b54937db02f
RudychukDmytro <dmytrorudychuk@gmail.com>
2023-09-07 16:36:38 +0300

    create templates

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask
-
+from flask import (Flask,
+                   render_template,
+                  )
+from markupsafe import escape
 
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "Hello world!"
+def index():
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
